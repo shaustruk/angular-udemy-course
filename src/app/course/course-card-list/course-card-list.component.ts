7e4336193dedeaabd34da7247257867fef8a972a
ShaustrukE <es@banksiaglobal.com>
2022-11-29 22:58:16 +0300

    refactor: fix prettier

---
+++ src/app/course/course-card-list/course-card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card-list',
+  templateUrl: './course-card-list.component.html',
+  styleUrls: ['./course-card-list.component.scss'],
+})
+export class CourseCardListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
